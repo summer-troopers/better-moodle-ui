29b7b51351a381570d6ff1a4b626f545655311fb
Maria Turuta <mariaturuta@gmail.com>
2018-07-30 18:11:43 +0300

    created dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { CoreModule } from '@core/core.module';
-import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
-import { DashboardModule } from '@modules/dashboard/dashboard.module';
+import {CoreModule} from '@core/core.module';
+import {AppComponent} from './app.component';
+import {appRoutes} from './app.routes';
+import {DashboardModule} from '@modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
