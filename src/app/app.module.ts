0c1971706ab2e0ef9fdc66b72a15645015a057c9
Maria Turuta <mariaturuta@gmail.com>
2018-07-31 14:08:30 +0300

    Added collaps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {CoreModule} from '@core/core.module';
+
 import {AppComponent} from './app.component';
 import {appRoutes} from './app.routes';
 import {DashboardModule} from '@modules/dashboard/dashboard.module';
 import {HomeModule} from '@modules/home/home.module';
-
 
 @NgModule({
   declarations: [
