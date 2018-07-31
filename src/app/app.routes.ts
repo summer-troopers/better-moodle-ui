61a761a0966d94f2736d21ba1dfd91f2e0ee3fd6
Cojocaru Andreea - Cristina <andreacojocaru@gmail.com>
2018-07-31 11:02:25 +0300

    fixed paths to be relative in students module (#8)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {DashboardPageComponent} from '@modules/dashboard/containers/dashboard-page/dashboard-page.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from '@modules/dashboard/containers/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
   {
