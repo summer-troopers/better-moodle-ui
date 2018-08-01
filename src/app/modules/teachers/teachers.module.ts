2a9eebbf55d154c748cd07a37941ef850ae1a117
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-01 09:31:07 +0300

    test

--- src/app/modules/teachers/teachers.module.ts
+++ src/app/modules/teachers/teachers.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeacherDetailsPageComponent, TeachersPageComponent } from '@modules/teachers/containers';
+import { AddTeacherComponent } from './components/add-teacher/add-teacher.component';
 
 import { teacherRoutes } from './teachers.routes';
 
 @NgModule({
   imports: [
     CommonModule,
+    AddTeacherComponent,
     teacherRoutes
   ],
-  declarations: [TeacherDetailsPageComponent, TeachersPageComponent]
+  declarations: [TeacherDetailsPageComponent, TeachersPageComponent, AddTeacherComponent]
 })
 export class TeachersModule { }
