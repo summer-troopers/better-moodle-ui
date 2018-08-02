98c29efe729e4848a6d1fa9c7a1362b30706e250
vtamazlicari <cronostw@gmail.com>
2018-08-02 15:28:05 +0300

    [ADD][TASK-4044] create new methode from insert login-data in localstorage and modify evironment url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'http://10.177.106.10:80/api/v1'
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
