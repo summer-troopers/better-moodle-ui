249018fea272afbda083ebbbb14b1003a627f564
Vadim <vbraduleac@pentalog.com>
2018-08-06 22:54:42 +0300

    Update backend-api.service.ts

--- src/app/core/services/api/backend-api.service.ts
+++ src/app/core/services/api/backend-api.service.ts
@@ -6,8 +6,6 @@ import { Observable } from 'rxjs';
 import { LocalStorageService } from '@shared/services/local-storage.service';
 import { TOKEN_STORAGE_KEY } from '@shared/constants';
 
-/* tslint:disable max-line-length */
-
 @Injectable({
   providedIn: 'root'
 })
