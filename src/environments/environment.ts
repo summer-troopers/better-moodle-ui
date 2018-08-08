b255b3f34d1ae669ce8060202c6816524de47e3c
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-08 08:43:22 +0300

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,11 @@
 
 export const environment = {
   production: false,
+<<<<<<< HEAD
   apiUrl: 'http://localhost:80/api/v1'
+=======
+  apiUrl: 'http://10.177.106.10:80/api/v1'
+>>>>>>> update env
 };
 
 /*
