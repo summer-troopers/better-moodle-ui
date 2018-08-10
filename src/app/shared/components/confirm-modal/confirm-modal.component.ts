292ca19e7b6f8904fa359cfc8b0baba2470bd686
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-10 18:52:32 +0300

    added lines

--- src/app/shared/components/confirm-modal/confirm-modal.component.ts
+++ src/app/shared/components/confirm-modal/confirm-modal.component.ts
@@ -1,20 +1,24 @@
 import { Component, EventEmitter } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-confirm-modal',
   templateUrl: './confirm-modal.component.html'
 })
 export class ConfirmModalComponent {
-
   deleted: EventEmitter<any> = new EventEmitter<any>();
   message: string;
+
   constructor(public bsModalRef: BsModalRef,
     private router: Router) { }
+
   confirm() {
     this.deleted.emit();
   }
+
   decline(): void {
     this.bsModalRef.hide();
   }
+
 }
