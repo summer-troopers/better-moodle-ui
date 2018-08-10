5d194dcf8b2e1d375b7596d2d71e88495e81381a
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-10 18:26:08 +0300

    confirm modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocalAlertComponent } from '@shared/componets/alert/local-alert/local-alert.component';
 import { AlertComponent } from '@shared/componets/alert/alert.component';
+import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LocalAlertComponent, AlertComponent],
-  exports: [LocalAlertComponent, AlertComponent]
+  entryComponents: [ConfirmModalComponent],
+  declarations: [LocalAlertComponent, AlertComponent, ConfirmModalComponent],
 })
 export class SharedModule { }
