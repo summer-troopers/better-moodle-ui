6d75697af021f8b96cdfab03718d57bce4f554ea
vtamazlicari <cronostw@gmail.com>
2018-08-13 10:36:56 +0300

    add password length constant

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -2,3 +2,4 @@ export const TOKEN_STORAGE_KEY = 'authorization';
 export const USER_STORAGE_KEY = 'user';
 export const TEACHERS_URL = 'teachers';
 export const CONFIRM_MODAL_TIMEOUT = 2000;
+export const MIN_PASSWORD_LENGTH = 5;
