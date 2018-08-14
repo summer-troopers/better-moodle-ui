331bf5951319e55b26b51da668927acbdac511a4
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-14 08:48:01 +0300

    changed few errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from '@core/core.module';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { NgHttpLoaderModule } from 'ng-http-loader';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
@@ -18,7 +17,6 @@ import { SharedModule } from '@shared/shared.module'
     ModalModule.forRoot(),
     CoreModule,
     SharedModule,
-    NgHttpLoaderModule,
     HomeModule,
     appRoutes
   ],
