6ec972f029a1ae69e11154fa2ab1fc3d566eeace
Maria Turuta <mariaturuta@gmail.com>
2018-08-15 16:21:02 +0300

    initialized teachers-dashboard view courses

--- src/app/modules/dashboard/components/my-courses/my-courses.component.ts
+++ src/app/modules/dashboard/components/my-courses/my-courses.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {LABTASKS, GROUPS_URL, TEACHERS_URL, COURSES_URL} from '@shared/constants';
+import {mergeMap} from 'rxjs/operators';
+
+import {CrudService} from '@shared/services/crud/crud.service';
+import Course from '@shared/models/course';
 
 @Component({
   selector: 'app-my-courses',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCoursesComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  course: Course;
 
-  ngOnInit() {
+  constructor(private crudService: CrudService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.teacherId = params['teacherId'];
+      this.crudService.getItem(COURSES_URL, this.teacherId)
+        .pipe(
+          mergeMap((course: Course) => {
+            this.course = course;
+            return this.crudService.getItem(TEACHERS_URL, course.teacherId.toString()),
+          }),
+        )
+        .subscribe(teacher => this.firstName = teacher.firstName);
+    });
+  }
 }
