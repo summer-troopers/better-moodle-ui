7c460b9a929d11173c141887bb6731ecaa7f983e
Maria Turuta <mariaturuta@gmail.com>
2018-08-16 11:32:58 +0300

    done teacher view for students dashboard

--- src/app/modules/dashboard/components/my-specialties/my-specialties.component.ts
+++ src/app/modules/dashboard/components/my-specialties/my-specialties.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject, throwError } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
+
+import Specialty from '@shared/models/group';
+import { SPECIALTIES_URL } from '@shared/constants';
+import { Alert, AlertType } from '@shared/models/alert';
+import { DashboardService } from '@modules/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-my-specialties',
   templateUrl: './my-specialties.component.html',
   styleUrls: ['./my-specialties.component.scss']
 })
-export class MySpecialtiesComponent implements OnInit {
+export class MySpecialtiesComponent implements OnInit, OnDestroy {
+  id: string;
+  specialties: Array<Specialty> = [];
+  @Input() user;
 
-  constructor() { }
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  ngOnInit() {
+  alerts: Alert[] = [];
+
+  constructor(private dashboardService: DashboardService) {
   }
 
+  ngOnInit() {
+    this.getAllSpecialties();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  getAllSpecialties() {
+    const userId = this.user.id;
+    this.dashboardService.getItemsofTeacher(SPECIALTIES_URL, userId)
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError((error) => {
+          this.alerts.push({type: AlertType.Error, message: error});
+          return throwError(error);
+        })
+      )
+      .subscribe((specialties) => {
+        this.specialties = specialties;
+      });
+  }
 }
--- src/app/modules/dashboard/components/my-teachers/my-teachers.component.ts
+++ src/app/modules/dashboard/components/my-teachers/my-teachers.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject, throwError } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
+
+import Teacher from '@shared/models/group';
+import { TEACHERS_URL } from '@shared/constants';
+import { Alert, AlertType } from '@shared/models/alert';
+import { DashboardService } from '@modules/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-my-teachers',
   templateUrl: './my-teachers.component.html',
   styleUrls: ['./my-teachers.component.scss']
 })
-export class MyTeachersComponent implements OnInit {
+export class MyTeachersComponent implements OnInit, OnDestroy {
+  id: string;
+  teachers: Array<Teacher> = [];
+  @Input() user;
 
-  constructor() { }
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  ngOnInit() {
+  alerts: Alert[] = [];
+
+  constructor(private dashboardService: DashboardService) {
   }
 
+  ngOnInit() {
+    this.getAllTeachers();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  getAllTeachers() {
+    const userId = this.user.id;
+    this.dashboardService.getItemsofStudent(TEACHERS_URL, userId)
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError((error) => {
+          this.alerts.push({type: AlertType.Error, message: error});
+          return throwError(error);
+        })
+      )
+      .subscribe((teachers) => {
+        this.teachers = teachers;
+      });
+  }
 }
