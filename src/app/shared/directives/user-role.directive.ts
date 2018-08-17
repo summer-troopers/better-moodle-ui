fd7d2e7c666ccfe3ed5c59a61f320bbf5b84accd
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-17 16:26:33 +0300

    add directive

--- src/app/shared/directives/user-role.directive.ts
+++ src/app/shared/directives/user-role.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, OnInit, ElementRef, Renderer } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef } from '@angular/core';
 
 import { UserService } from '@shared/services/user/user.service';
 import { USER_STORAGE_KEY } from '@shared/constants';
@@ -6,24 +6,19 @@ import { USER_STORAGE_KEY } from '@shared/constants';
 @Directive({
   selector: '[appHasRole]'
 })
-export class StudentRoleDirective implements OnInit {
+export class UserRoleDirective implements OnInit {
 
   @Input() appHasRole: string;
 
-  isVisible = false;
-
   constructor(private userService: UserService,
-    private el: ElementRef,
-    private renderer: Renderer) {
+    private elementRef: ElementRef) {
   }
 
   ngOnInit() {
-
     const user = this.userService.getUserLocalStorage(USER_STORAGE_KEY).userRole;
-    console.log(user);
     if (user === 'teacher' || user === 'student') {
-      this.renderer.setElementProperty(this.el.nativeElement.style, 'visibility', 'hidden');
+      const child: HTMLElement = this.elementRef.nativeElement;
+      child.parentNode.removeChild(child);
     }
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LocalAlertComponent } from '@shared/components/alert/local-alert/local-alert.component';
 import { AlertComponent } from '@shared/components/alert/alert.component';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
-import { StudentRoleDirective } from '@shared/directives/student-role.directive';
+import { UserRoleDirective } from '@shared/directives/user-role.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   entryComponents: [ConfirmModalComponent],
-  declarations: [LocalAlertComponent, AlertComponent, ConfirmModalComponent, StudentRoleDirective],
-  exports: [LocalAlertComponent, AlertComponent, StudentRoleDirective]
+  declarations: [LocalAlertComponent, AlertComponent, ConfirmModalComponent, UserRoleDirective],
+  exports: [LocalAlertComponent, AlertComponent, UserRoleDirective]
 })
 export class SharedModule { }
