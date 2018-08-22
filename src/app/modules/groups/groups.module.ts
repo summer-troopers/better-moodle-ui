3f9a1614eb45bfb3ddf05d7f03467592d860c9c8
IHarea050197 <40391264+mrRobot157@users.noreply.github.com>
2018-08-22 14:55:25 +0300

    Added user role displaying modals , and refactored some html code for … (#79)
    * Resolved a conflict
    
    * Resolved some problems on prod
    
    * Switched to http://localhost:80

--- src/app/modules/groups/groups.module.ts
+++ src/app/modules/groups/groups.module.ts
@@ -6,7 +6,7 @@ import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 
-import { groupsRoutes } from './groups.routes';
+import { groupsRoutes } from '@modules/groups/groups.routes';
 import { GroupDetailsPageComponent, GroupsPageComponent } from './containers';
 import { AddGroupModalComponent } from './components/add-group-modal/add-group-modal.component';
 import { EditGroupModalComponent } from './components/edit-group-modal/edit-group-modal.component';
