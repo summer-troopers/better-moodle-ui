69aa83e8147aa3594415aad64d779e975b6f12dc
Vadim <vbraduleac@pentalog.com>
2018-08-24 15:53:40 +0300

    Style for home page/ less for others (#99)
    * fix master err with styles

--- src/app/modules/home/home-page/home-page.component.ts
+++ src/app/modules/home/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CrudService } from '@shared/services/crud/crud.service';
+import { TEACHERS_URL, STUDENTS_URL, ADMINS_URL } from '@shared/constants'
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  totalAdmins: any;
+  totalTeachers: any;
+  totalStudents: any;
+
+  constructor(private crud: CrudService) { }
 
   ngOnInit() {
+    this.getAllAdmins();
+    this.getAllStudents();
+    this.getAllTeachers();
+  }
+
+  getAllTeachers() {
+    return this.crud.getItems(TEACHERS_URL).subscribe(
+      teacher => {
+        this.totalTeachers = teacher.length;
+      }
+    );
+  }
+
+  getAllStudents() {
+    return this.crud.getItems(STUDENTS_URL).subscribe(
+      student => {
+        this.totalStudents = student.length;
+      }
+    );
+  }
+
+  getAllAdmins() {
+    return this.crud.getItems(ADMINS_URL).subscribe(
+      admin => {
+        this.totalAdmins = admin.length;
+      }
+    );
   }
 
 }
