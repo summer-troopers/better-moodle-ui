aad2be1b1143fb7c7f2e3790464a948116677cc3
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-24 11:52:59 +0300

    fix router link

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [HomePageComponent]
 })
