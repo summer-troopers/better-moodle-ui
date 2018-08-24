09c4606f0a039ee2436eb6d5c27523bb9bc1dd43
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-08-24 08:36:15 +0300

    fix

--- src/app/core/interceptors/errors-interceptor/errors-interceptor.service.ts
+++ src/app/core/interceptors/errors-interceptor/errors-interceptor.service.ts
@@ -3,7 +3,6 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { AuthenticationService } from '@modules/authentication/authentication.service';
 import { AlertService } from '@shared/services/alert/alert.service';
 
 @Injectable({
@@ -11,14 +10,13 @@ import { AlertService } from '@shared/services/alert/alert.service';
 })
 export class ErrorsInterceptorService implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService,
-    private alertService: AlertService) { }
+  constructor(private alertService: AlertService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if (err.status === 403 && this.authenticationService.isAuthenticated()) {
+      if (err.status === 403) {
         this.alertService.error('You don`t have permissions to do this action!');
-      } else if (err.status === 409 && this.authenticationService.isAuthenticated()) {
+      } else if (err.status === 409) {
         this.alertService.error('This method could not be performed on this resource because another resourse depend on this one.');
       }
       const error = err.error.message || err.statusText;
