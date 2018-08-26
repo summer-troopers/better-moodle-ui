89558070f7acd08ba7aa2c4cd6110f91fe857c38
Vadim <vbraduleac@pentalog.com>
2018-08-26 20:28:47 +0300

    Update index.ts

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -12,8 +12,8 @@ export const LABTASK_URL = 'lab_tasks';
 export const LABCOMMENT_URL = 'lab_comments';
 export const HOME_URL = 'home';
 export const AUTHENTICATION_URL = 'auth';
-export const NUMBER_ITEMS_PAGE = 30;
-export const MAX_SIZE_PAGINATION = 15;
+export const NUMBER_ITEMS_PAGE = 10;
+export const MAX_SIZE_PAGINATION = 5;
 export const STUDENTS_URL = 'students';
 export let MODAL_OPTIONS: {} = {
   initialState: {},
